2dbbe98c0d21b3eb73b5823d3509acb0e652a8c5
torafugu <yoshihito.kaneda@gmail.com>
2024-05-19 12:39:59 +0900

    second commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+mod generator;
+pub fn print_random_number() {
+    let n = generator::gen_ran();
+    println!("Random u8: {}", n);
 }
